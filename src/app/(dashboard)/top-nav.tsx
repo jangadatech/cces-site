57a4f0d40ed94d280c85c4b0a23ccca855a15b30
venilson2 <venilson2009@hotmail.com>
2023-07-24 17:39:35 -0300

    refactor: ajustes top-bar

--- src/app/(dashboard)/top-nav.tsx
+++ src/app/(dashboard)/top-nav.tsx
@@ -5,21 +5,39 @@ import { styled } from '@mui/material/styles';
 import AppBar from '@mui/material/AppBar';
 import Toolbar from '@mui/material/Toolbar';
 import Avatar from '@mui/material/Avatar';
+import { useSession } from 'next-auth/react';
 
 const TopNavWrapper = styled(AppBar)(({ theme }) => ({
   backgroundColor: 'transparent',
   width: '100%',
   marginLeft: 0,
-  color: theme.palette.text.primary, 
-  boxShadow: 'none'
+  color: theme.palette.text.primary,
+  boxShadow: 'none',
 }));
 
+const UserInfoContainer = styled('div')({
+  display: 'flex',
+  alignItems: 'center',
+});
+
+const UserName = styled('span')({
+  marginLeft: '8px',
+  fontWeight: 'bold',
+});
+
 const TopNav = () => {
+  const {data: session, status} = useSession()
+  console.log(session)
+  const username = 'John Doe';
+
   return (
     <TopNavWrapper position="fixed">
       <Toolbar>
         <div style={{ flexGrow: 1 }} />
-        <Avatar alt="Remy Sharp" src="https://www.w3schools.com/howto/img_avatar.png" />
+        <UserInfoContainer>
+          <Avatar alt={username} src="https://www.w3schools.com/howto/img_avatar.png" />
+          <UserName>{username}</UserName>
+        </UserInfoContainer>
       </Toolbar>
     </TopNavWrapper>
   );
