85f4aac22c03a365cba4b0e107f45eafa659f835
venilson2 <venilson2009@hotmail.com>
2023-07-28 11:40:34 -0300

    feat(Vehicle): implementacao da model

---
+++ src/app/models/Vehicle.ts
@@ -0,0 +1,13 @@
+import mongoose from "mongoose";
+
+const VehicleSchema = new mongoose.Schema({
+    plate: { type: String, required: true },
+    active: { type: Boolean, default: true  },
+    prefix: { type: String, required: true },
+    vehicleType: { type: mongoose.Schema.Types.ObjectId, ref: "VehicleType", required: true },
+    created_at: { type: Date, default: Date.now },
+    updated_at: { type: Date, default: null }
+  });
+  
+  const Vehicle = mongoose.models.Vehicle || mongoose.model('Vehicle', VehicleSchema);
+  export default Vehicle 
