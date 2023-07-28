6a489ec714b6ba4823b5a7b6adcab743a99dbff2
venilson2 <venilson2009@hotmail.com>
2023-07-28 11:39:27 -0300

    refactor(User): ajuste na model

--- src/app/models/User.ts
+++ src/app/models/User.ts
@@ -2,8 +2,10 @@ import mongoose from "mongoose";
 
 const userSchema = new mongoose.Schema({
     username: { type: String, unique: true },
+    full_name: { type: String, required: true },
+    active: { type: Boolean, required: true },
     password: String,
-    profile: String,
+    profile: { type: String, enum: ['admin', 'traffic-control', 'human-resources'], required: true },
     created_at: { type: Date, default: Date.now },
     updated_at: { type: Date, default: null }
   });
