10a0ce16efb2d184968950296da18696eb48e531
PhoenixBytee <venilson2009@hotmail.com>
2023-08-27 10:32:43 -0300

    feat(drivers): componente para criar e atualizar drivers

---
+++ src/app/(dashboard)/drivers/[id]/page.tsx
@@ -0,0 +1,52 @@
+'use client'
+
+import useFetch from '@/hook/useFetch';
+import IDriver from '@/interfaces/IDriver';
+import FormDrivers from '@/sections/drivers/FormDrivers';
+import React, { useState } from 'react';
+import { toast } from "react-toastify";
+import 'react-toastify/dist/ReactToastify.css';
+
+interface SearchParamsURL {
+  params: {
+      id: string;
+  };
+  searchParams: IDriver;
+}
+
+const UpdateDriver = (url: SearchParamsURL) => {
+
+  const { searchParams } = url;
+  const data = searchParams;
+
+  const inputOutputInit: IDriver = {
+    name: data.name,
+    full_name: data.full_name,
+    enrollment: data.enrollment,
+  }
+
+  const [isLoading, setIsLoading] = useState(false);
+  const { response: drivers, loading, error, request } = useFetch<IDriver[]>('/api/drivers');
+
+  const handleSubmit = async (values: IDriver) => {
+    try {
+      await request('put', values);
+      toast.success('Dados Atualizados com sucesso!', { theme: 'colored' });
+    } catch (error) {
+      console.error(error);
+      toast.error('Erro ao Atualizar dados!', { theme: 'colored' });
+    }
+    setIsLoading(false);
+  };
+
+  return (
+    <>
+      <title>
+        Atualizar Motorista | CCES
+      </title>
+      <FormDrivers handleSubmit={handleSubmit} typeText="Atualizar" initialValues={inputOutputInit} />;
+    </>
+  )
+};
+
+export default UpdateDriver;
---
+++ src/app/(dashboard)/drivers/create/page.tsx
@@ -0,0 +1,43 @@
+'use client'
+
+import useFetch from '@/hook/useFetch';
+import IDriver from '@/interfaces/IDriver';
+import FormDrivers from '@/sections/drivers/FormDrivers';
+import React, { useState } from 'react';
+import { toast } from "react-toastify";
+import 'react-toastify/dist/ReactToastify.css';
+
+const CreateDriver = () => {
+
+  const inputOutputInit: IDriver = {
+    name: '',
+    full_name: '',
+    enrollment: '',
+  }
+
+  const [isLoading, setIsLoading] = useState(false);
+  const { response: drivers, loading, error, request } = useFetch<IDriver[]>('/api/drivers');
+
+
+  const handleSubmit = async (values: any) => {
+    try {
+      await request('post', values);
+      toast.success('Dados salvo com sucesso!', { theme: 'colored' });
+    } catch (error) {
+      console.error(error);
+      toast.error('Erro ao salvar dados!', { theme: 'colored' });
+    }
+    setIsLoading(false);
+  };
+
+  return (
+    <>
+      <title>
+        Cadastrar Motorista | CCES
+      </title>
+      <FormDrivers handleSubmit={handleSubmit} typeText="Cadastrar" initialValues={inputOutputInit} />;
+    </>
+  )
+};
+
+export default CreateDriver;
